6694f61d3d45f1a77b7cf4bd2e5802f036909ed7
Pakathon singpuntha <120086620+pakathon154@users.noreply.github.com>
2024-12-08 10:52:55 +0700

    Add files via upload

---
+++ ex1.js
@@ -0,0 +1,9 @@
+  console.log(findPerimeter(6, 7)); 
+  console.log(findPerimeter(20, 10));  
+  console.log(findPerimeter(2, 9));  
+
+
+  function findPerimeter(L, W) {
+    return 2 * (L + W);
+  }
+  
---
+++ ex2.js
@@ -0,0 +1,11 @@
+console.log(equation("1+1"));          
+  console.log(equation("7*4-2"));        
+  console.log(equation("1+1+1+1+1"));  
+  
+
+function equation(str) {
+    return eval(str);
+  }
+  
+
+  
